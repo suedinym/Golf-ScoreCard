badfdf1f6d853c6723db2cd61eaf8647fc117de7
suedinym <suedinym@gmail.com>
2015-11-15 10:32:17 -0500

    Added array for holes in main activity, then added the computation to display the holes in the list

--- app/src/main/java/com/suedinym/golfscorecard/MainActivity.java
+++ app/src/main/java/com/suedinym/golfscorecard/MainActivity.java
@@ -5,10 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends ListActivity {
+    private Hole[] mHoles = new Hole[18];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialize holes
+        int strokes = 0;
+        for (int i = 0; i < mHoles.length; i++){
+            mHoles[i] = new Hole("Hole" + (i+1)+ ";", strokes);
+        }
+
     }
 }
