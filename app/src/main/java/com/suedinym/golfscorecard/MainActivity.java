38401c1ce591ed609b8b14b5bbc83c292654290b
suedinym <suedinym@gmail.com>
2015-11-15 12:47:11 -0500

    Added shared preferences, but there's a problem and it won't add more than one stroke. It keeps them okand they survive stopping and restarting.

--- app/src/main/java/com/suedinym/golfscorecard/MainActivity.java
+++ app/src/main/java/com/suedinym/golfscorecard/MainActivity.java
@@ -1,10 +1,16 @@
 package com.suedinym.golfscorecard;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends ListActivity {
+    private static final String PREFS_FILE = "com.suedinym.golfscorecard.preferences";
+    private static final String KEY_STROKECOUNT = "key_strokecount";
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
     private Hole[] mHoles = new Hole[18];
     private ListAdapter mListAdapter;
 
@@ -12,15 +18,27 @@ public class MainActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mSharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
+mEditor = mSharedPreferences.edit();
         // initialize holes
         int strokes = 0;
         for (int i = 0; i < mHoles.length; i++){
+            strokes = mSharedPreferences.getInt(KEY_STROKECOUNT + i, 0);
             mHoles[i] = new Hole("Hole " + (i+1)+ ":", strokes);
         }
       mListAdapter = new ListAdapter(this, mHoles);
         setListAdapter(mListAdapter);
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        for (int i = 0; i<mHoles.length; i++){
+            mEditor.putInt(KEY_STROKECOUNT + i, mHoles[i].getmStrokeCount());
+
+        }
+        mEditor.apply();
+    }
 }
-//// TODO: 11/15/15 find out what is putting the number smack up next to the Hole and also adding 
-// // TODO: 11/15/15 semi colon, and fix that part
+//// TODO: 11/15/15 figure out why it will only add one 
